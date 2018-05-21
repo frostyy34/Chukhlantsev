7c14f011937359b968f5909d0b7f571b9f370898
frostyy34 <39503264+frostyy34@users.noreply.github.com>
2018-05-22 01:34:13 +0400

    point3d

---
+++ Point3d/Lab1.java
@@ -0,0 +1,35 @@
+package lab;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Lab1 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		double[][] mas = new double[3][3];
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+				System.out.println("Vvedite " + ((j == 0) ? "x":"") + ((j == 1) ? "y":"") + ((j == 2) ? "z":"") + " dlya tochki " + (i+1));
+				if(in.hasNextDouble())
+					mas[i][j] = in.nextDouble();
+				else
+					System.exit(0);
+			}
+		}
+		in.close();
+		Point3d p1 = new Point3d(mas[0][0], mas[0][1], mas[0][2]);
+		Point3d p2 = new Point3d(mas[1][0], mas[1][1], mas[1][2]);
+		Point3d p3 = new Point3d(mas[2][0], mas[2][1], mas[2][2]);
+		double answer = heron(p1, p2, p3);
+		System.out.println("Ploshad treugolnika po formule Gerona ravna: " + answer);
+	}
+
+	public static double heron(Point3d p1, Point3d p2, Point3d p3) {
+		double a = p1.distanceTo(p2);
+		double b = p2.distanceTo(p3);
+		double c = p3.distanceTo(p1);
+		double p = (a+b+c)/2;
+		return Math.sqrt(p*(p-a)*(p-b)*(p-c));
+	}
+}
---
+++ Point3d/Point3d.java
@@ -0,0 +1,46 @@
+package lab;
+
+import java.lang.Math;
+
+public class Point3d {
+	private double xCoord, yCoord, zCoord;
+	// ==============================
+	public Point3d(double x, double y, double z) {
+		xCoord = x;
+		yCoord = y;
+		zCoord = z;
+	}
+	public Point3d() {
+		xCoord = yCoord = zCoord = 0;
+	}
+	// ==============================
+	public double getX() {
+		return xCoord;
+	}
+	public double getY() {
+		return yCoord;
+	}
+	public double getZ() {
+		return zCoord;
+	}
+	// ==============================
+	public void setX(double x) {
+		this.xCoord = x;
+	}
+	public void setY(double y) {
+		this.yCoord = y;
+	}
+	public void setZ(double z) {
+		this.zCoord = z;
+	}
+	// ==============================
+	public boolean equals(Point3d obj) {
+		if( (this.getX() == obj.getX()) && (this.getY() == obj.getY()) && (this.getZ() == obj.getZ()) )
+			return true;
+		return false;
+	}
+	// ==============================
+	public double distanceTo(Point3d obj) {
+		return Math.sqrt(Math.pow(obj.getX()-this.getX(),2) + Math.pow(obj.getY()-this.getY(),2) + Math.pow(obj.getZ()-this.getZ(),2));
+	}
+}
